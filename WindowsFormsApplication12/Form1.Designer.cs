07adc141cb53ff1e4b17b60499e2d68dd9bf3120
brklcc <brkklcc.7@gmail.com>
2018-12-20 14:34:49 +0300

    görüntü

---
+++ WindowsFormsApplication12/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Goruntuisleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelleddurum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(663, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(117, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(244, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kamera Seçiniz";
+            // 
+            // labelleddurum
+            // 
+            this.labelleddurum.AutoSize = true;
+            this.labelleddurum.Location = new System.Drawing.Point(361, 29);
+            this.labelleddurum.Name = "labelleddurum";
+            this.labelleddurum.Size = new System.Drawing.Size(35, 13);
+            this.labelleddurum.TabIndex = 7;
+            this.labelleddurum.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 581);
+            this.Controls.Add(this.labelleddurum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelleddurum;
+    }
+}
+
---
+++ WindowsFormsApplication12/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision;
+using AForge.Vision.Motion;
+
+namespace Goruntuisleme
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice kameraAygit;
+        private FilterInfoCollection webcamAygit;
+        bool LEDdurumu = false;
+        SerialPort seriPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            seriPort = new SerialPort();
+            seriPort.PortName = "COM3";
+            seriPort.BaudRate = 9600;
+            seriPort.Open();
+        }
+
+        //*****************************************
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (seriPort.IsOpen) seriPort.Close();
+            if (kameraAygit != null && kameraAygit.IsRunning)
+            {
+                kameraAygit.SignalToStop();
+                kameraAygit = null;
+            }
+        }
+
+        //*****************************************
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelleddurum.Visible = false;
+            webcamAygit = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in webcamAygit)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name); // WebCamleri listele
+                comboBox1.SelectedIndex = 0;
+            }
+
+        }
+
+        //*****************************************
+
+        void cam_goruntu_new_frame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            var filter2 = new Mirror(false, true);
+            filter2.ApplyInPlace(image1);
+            var filter = new Mirror(false, true);
+            filter.ApplyInPlace(image);
+            pictureBox1.Image = image;
+            
+
+            EuclideanColorFiltering filter1 = new EuclideanColorFiltering();
+            filter1.CenterColor = new RGB(Color.FromArgb(10, 14, 200));
+            filter1.Radius = 100;
+            filter1.ApplyInPlace(image1);
+            nesnebul(image1);
+
+
+
+        }
+
+        //*****************************************
+
+        public void nesnebul(Bitmap image)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 2;
+            blobCounter.MinHeight = 2;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+            Grayscale griFiltre = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap griImage = griFiltre.Apply(image);
+            blobCounter.ProcessImage(griImage);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            pictureBox2.Image = image;
+            foreach (Rectangle recs in rects)
+            {
+                if (rects.Length > 0)
+                {
+                    Rectangle objectRect = rects[0];
+                    //Graphics g = Graphics.FromImage(image);
+                    Graphics g = pictureBox1.CreateGraphics();
+                    using (Pen pen = new Pen(Color.FromArgb(252, 3, 26), 2))
+                    {
+                        g.DrawRectangle(pen, objectRect);
+                    }
+                    //Cizdirilen Dikdörtgenin Koordinatlari aliniyor.
+                    int objectX = objectRect.X + (objectRect.Width / 2);
+                    int objectY = objectRect.Y + (objectRect.Height / 2);
+                    int saklax = objectX;
+                    int saklay = objectY;
+                    g.DrawString(objectX.ToString() + "X" + objectY.ToString(), new Font("Arial", 12), Brushes.Black, new System.Drawing.Point(560, 10));
+                    g.Dispose();
+                    int[] dizi = new int[100];
+                    //bölge 1
+                    if (saklax < 213 && saklay < 160 && saklax > 0 && saklay > 0)
+                    {
+                        seriPort.Write("1");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 426 && saklay < 160 && saklax > 213 && saklay > 0)
+                    {
+                        seriPort.Write("2");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 640 && saklay < 160 && saklax > 426 && saklay > 0)
+                    {
+                        seriPort.Write("3");
+                        LEDdurumu = true;
+
+                    }
+                    if (saklax < 213 && saklay < 320 && saklax > 0 && saklay > 160)
+                    {
+                        seriPort.Write("4");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 426 && saklay < 320 && saklax > 213 && saklay > 160)
+                    {
+                        seriPort.Write("5");
+                        LEDdurumu = true;
+
+                    }
+                    if (saklax < 640 && saklay < 320 && saklax > 426 && saklay > 160)
+                    {
+                        seriPort.Write("6");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 213 && saklay < 480 && saklax > 0 && saklay > 320)
+                    {
+                        seriPort.Write("7");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 426 && saklay < 480 && saklax > 213 && saklay > 320)
+                    {
+                        seriPort.Write("8");
+                        LEDdurumu = true;
+                    }
+                    if (saklax < 640 && saklay < 480 && saklax > 426 && saklay > 320)
+                    {
+                        seriPort.Write("9");
+                        LEDdurumu = true;
+                    }
+
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kameraAygit = new VideoCaptureDevice(webcamAygit[comboBox1.SelectedIndex].MonikerString);//webcam listesinden kafadan birinciyi al diyoruz.
+            kameraAygit.NewFrame += new NewFrameEventHandler(cam_goruntu_new_frame);
+            kameraAygit.Start();
+        }
+
+    }
+}
+
+
